04b6921f79aed60499731d525ff4040a66897e1d
Jamie Larchin <jamielarchin@gmail.com>
2019-04-12 08:28:52 +1000

    Add api endpoints that use lowdb

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ const app = express();
 const jwt = require("express-jwt");
 const jwtAuthz = require("express-jwt-authz");
 const jwksRsa = require("jwks-rsa");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({
+    habits: [
+        {
+            id: 1,
+            name: "My first habit"
+        }
+    ]
+}).write();
 
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
     throw new Error(
@@ -17,6 +32,7 @@ const corsOptions = {
     origin: "http://localhost:3000"
 };
 
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 const checkJwt = jwt({
@@ -33,21 +49,33 @@ const checkJwt = jwt({
     issuer: `https://${process.env.AUTH0_DOMAIN}/`,
     algorithms: ["RS256"]
 });
+const checkScopes = jwtAuthz(["read:habits"]);
 
-const checkScopes = jwtAuthz(["read:messages"]);
-
-app.get("/api/public", (req, res) => {
-    res.json({
-        message:
-            "Hello from a public endpoint! You don't need to be authenticated to see this."
-    });
+app.get("/api/habits", checkJwt, (req, res) => {
+    const habits = db.get("habits").value();
+    res.json(habits);
 });
 
-app.get("/api/private", checkJwt, (req, res) => {
-    res.json({
-        message:
-            "Hello from a private endpoint! You need to be authenticated to see this."
-    });
+app.get("/api/habits/:id", checkJwt, (req, res) => {
+    const { id } = req.params;
+    const habit = db
+        .get("habits")
+        .find({ id: parseInt(id, 10) })
+        .value();
+    res.json(habit);
+});
+
+app.post("/api/habits", checkJwt, (req, res) => {
+    const dbCount = db
+        .get("habits")
+        .size()
+        .value();
+
+    const habits = db
+        .get("habits")
+        .push({ id: dbCount + 1, name: req.body.name })
+        .write();
+    res.json(habits);
 });
 
 app.get("/api/private-scoped", checkJwt, checkScopes, (req, res) => {
